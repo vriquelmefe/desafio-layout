465e74ef79c91e418ab087a48467cfcee4e3ea03
“Vanessa <vriquelmefe@gmail.com>
2021-09-03 00:22:04 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,71 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app app>
+    <v-card
+      color="grey lighten-4"
+      dense
+    >
+      <v-toolbar  >
+        <v-app-bar-nav-icon  @click.stop="drawer = !drawer" ></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Mi App</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn text>Inicio</v-btn>
+          <v-btn text>Buscar</v-btn>
+          <v-btn text>Acerca de</v-btn>
+        </v-toolbar-items>
+      </v-toolbar>
+      <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      bottom
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-title>Inicio</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buscar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Acerca de</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+  </v-card>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
+  data: () => ({
+      drawer: false,
+      group: null,
+    }),
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+    watch: {
+      group () {
+        this.drawer = false
+      },
+    },
+};
+</script>
